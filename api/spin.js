b95397b6c06b19ccbc84c48d846597e9eb91072b
garaevdvd-ops <garaevdvd@gmail.com>
2026-01-15 22:50:36 +0500

    Create spin.js

---
+++ api/spin.js
@@ -0,0 +1,19 @@
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  try {
+    const response = await fetch('http://45.144.222.43:3000/spin', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(req.body)
+    });
+
+    const data = await response.json();
+    res.status(200).json(data);
+
+  } catch (e) {
+    res.status(500).json({ error: 'Server error' });
+  }
+}
